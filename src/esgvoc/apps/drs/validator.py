7b833893c5cbba64e4738e65a20e0c050556c3ca
Sébastien Gardoll <sebastien@gardoll.fr>
2025-01-14 15:47:59 +0100

    chore: add docstring

--- src/esgvoc/apps/drs/validator.py
+++ src/esgvoc/apps/drs/validator.py
@@ -23,10 +23,34 @@ from esgvoc.apps.drs.validation_report import (DrsValidationReport,
 
 
 class DrsValidator:
+    """
+    Valid directory, dataset id and file name against a given project DRS specifications.
+
+    :param project_id: The given project id
+    :type project_id: str
+    :param pedantic: Same as the option of GCC: transform warnings into errors
+    :type pedantic: bool
+    :param directory_specs: The directory DRS specifications of the given project.
+    :type directory_specs: dict
+    :param file_name_specs: The file name DRS specifications of the given project.
+    :type file_name_specs: dict
+    :param dataset_id_specs: The dataset id DRS specifications of the given project.
+    :type dataset_id_specs: dict
+    """
+
     # dict[project_id: dict[collection_id: set[token]]]
     _validated_token_cache: dict[str, dict[str, set[str]]] = dict()
     
     def __init__(self, project_id: str, pedantic: bool = False) -> None:
+        """
+        Constructor method.
+        
+        :param project_id: A project id.
+        :type project_id: str
+        :param pedantic: Turn warnings into errors. Default False.
+        :type pedantic: bool
+        """
+        
         self.project_id = project_id
         self.pedantic = pedantic
         project_specs: ProjectSpecs = projects.get_project_specs(project_id)
@@ -42,6 +66,68 @@ class DrsValidator:
                     raise ValueError(f'unsupported DRS specs type {specs.type}')
         if self.project_id not in DrsValidator._validated_token_cache:
             DrsValidator._validated_token_cache[self.project_id] = dict()
+    
+    def validate_directory(self, drs_expression: str) -> DrsValidationReport:
+        """
+        Validate a DRS directory expression.
+
+        :param drs_expression: A DRS directory expression.
+        :type drs_expression: str
+        :returns: A validation report.
+        :rtype: DrsValidationReport
+        """
+        
+        return self._validate(drs_expression, self.directory_specs)
+    
+    def validate_dataset_id(self, drs_expression: str) -> DrsValidationReport:
+        """
+        Validate a DRS dataset id expression.
+
+        :param drs_expression: A DRS dataset id expression.
+        :type drs_expression: str
+        :returns: A validation report.
+        :rtype: DrsValidationReport
+        """
+        
+        return self._validate(drs_expression, self.dataset_id_specs)
+
+    def validate_file_name(self, drs_expression: str) -> DrsValidationReport:
+        """
+        Validate a file name expression.
+
+        :param drs_expression: A DRS file name expression.
+        :type drs_expression: str
+        :returns: A validation report.
+        :rtype: DrsValidationReport
+        """
+        specs = self.file_name_specs
+        full_extension = specs.properties[constants.FILE_NAME_EXTENSION_SEPARATOR_KEY] + \
+                         specs.properties[constants.FILE_NAME_EXTENSION_KEY]
+        if drs_expression.endswith(full_extension):
+            drs_expression = drs_expression.replace(full_extension, '')
+            return self._validate(drs_expression, self.file_name_specs)
+        else:
+            issue = FileNameExtensionIssue(full_extension)
+            return self._create_report(drs_expression, [issue], [])
+
+    def validate(self, drs_expression: str, type: DrsType|str) -> DrsValidationReport:
+        """
+        Validate A DRS expression.
+
+        :param drs_expression: A DRS expression.
+        :type drs_expression: str
+        :param type: The type of the given DRS expression (directory, file_name or dataset_id)
+        :type type: DrsType
+        :returns: A validation report.
+        :rtype: DrsValidationReport
+        """
+        match type:
+            case DrsType.directory:
+                return self.validate_directory(drs_expression)
+            case DrsType.file_name:
+                return self.validate_file_name(drs_expression)
+            case DrsType.dataset_id:
+                return self.validate_dataset_id(drs_expression)
     
     def parse(self,
               drs_expression: str,
@@ -201,32 +287,6 @@ class DrsValidator:
                 errors.append(issue)
                 part_index += 1
         return self._create_report(drs_expression, errors, warnings)
-
-    def validate_directory(self, drs_expression: str) -> DrsValidationReport:
-        return self._validate(drs_expression, self.directory_specs)
-    
-    def validate_dataset_id(self, drs_expression: str) -> DrsValidationReport:
-        return self._validate(drs_expression, self.dataset_id_specs)
-
-    def validate_file_name(self, drs_expression: str) -> DrsValidationReport:
-        specs = self.file_name_specs
-        full_extension = specs.properties[constants.FILE_NAME_EXTENSION_SEPARATOR_KEY] + \
-                         specs.properties[constants.FILE_NAME_EXTENSION_KEY]
-        if drs_expression.endswith(full_extension):
-            drs_expression = drs_expression.replace(full_extension, '')
-            return self._validate(drs_expression, self.file_name_specs)
-        else:
-            issue = FileNameExtensionIssue(full_extension)
-            return self._create_report(drs_expression, [issue], [])
-
-    def validate(self, drs_expression: str, type: DrsType|str) -> DrsValidationReport:
-        match type:
-            case DrsType.directory:
-                return self.validate_directory(drs_expression)
-            case DrsType.file_name:
-                return self.validate_file_name(drs_expression)
-            case DrsType.dataset_id:
-                return self.validate_dataset_id(drs_expression)
 
 
 if __name__ == "__main__":
