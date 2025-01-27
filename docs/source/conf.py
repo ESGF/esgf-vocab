4061e177451a578e57efb27af7a9decb3758e41f
Sébastien Gardoll <sebastien@gardoll.fr>
2025-01-27 14:53:31 +0100

    doc: remake copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../../src'))
 
 project = 'ESGVOC'
-copyright = '2025, IPSL / ESPRI / CNRS'
+copyright = '2025, CNRS (IPSL/ESPRI)'
 author = 'S. Gardoll, G. Levavasseur, L. Troussellier.'
 BASE_URL = 'https://github.com/ESGF/esgf-vocab'
 
