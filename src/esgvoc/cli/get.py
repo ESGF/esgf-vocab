a711ba1c45f409dee1f0f9e84d2fc48368c52a3a
Sébastien Gardoll <sebastien@gardoll.fr>
2025-03-21 08:40:14 +0100

    refactor: find_terms_in_data_descriptor

--- src/esgvoc/cli/get.py
+++ src/esgvoc/cli/get.py
@@ -2,7 +2,8 @@
 from typing import Any
 from esgvoc.api.projects import get_all_collections_in_project, get_all_projects, \
     get_all_terms_in_collection, get_term_in_project, get_term_in_collection
-from esgvoc.api.universe import find_terms_in_data_descriptor, find_terms_in_universe, get_all_data_descriptors_in_universe, get_all_terms_in_data_descriptor
+from esgvoc.api.universe import find_terms_in_data_descriptor, find_terms_in_universe, \
+    get_all_data_descriptors_in_universe, get_all_terms_in_data_descriptor, get_term_in_data_descriptor
 from pydantic import BaseModel
 from requests import logging
 from rich.table import Table
@@ -29,7 +30,7 @@ def handle_universe(data_descriptor_id:str|None,term_id:str|None, options=None):
     _LOGGER.debug(f"Handling universe with data_descriptor_id={data_descriptor_id}, term_id={term_id}") 
 
     if data_descriptor_id and term_id:
-        return find_terms_in_data_descriptor(data_descriptor_id,term_id,options)
+        return get_term_in_data_descriptor(data_descriptor_id, term_id, options)
         # BaseModel|dict[str: BaseModel]|None:
 
     elif term_id:
