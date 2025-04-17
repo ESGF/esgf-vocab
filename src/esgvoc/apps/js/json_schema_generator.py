c3886720c551f65a802d0d68b2069941aebedacf
Sébastien Gardoll <sebastien@gardoll.fr>
2025-04-17 15:57:16 +0200

    fix: variant label & composite resolution

--- src/esgvoc/apps/js/json_schema_generator.py
+++ src/esgvoc/apps/js/json_schema_generator.py
@@ -2,7 +2,9 @@ import json
 from pathlib import Path
 from typing import Any
 
-from esgvoc.api import projects
+from sqlmodel import Session
+
+from esgvoc.api import projects, search
 from esgvoc.core.constants import PATTERN_JSON_KEY
 from esgvoc.core.db.models.project import Collection, TermKind
 from esgvoc.core.exceptions import EsgvocValueError
@@ -23,17 +25,28 @@ def _process_plain(collection: Collection, selected_field: str) -> list[str]:
     return result
 
 
-def _process_composite(collection: Collection) -> str:
+def _process_composite(collection: Collection, universe_session: Session,
+                       project_session: Session) -> str:
     result = ""
     for term in collection.terms:
-        if term.kind == TermKind.PATTERN:
-            result += term.specs[PATTERN_JSON_KEY]
-        else:
-            raise NotImplementedError(f'{term.kind} term is not supported yet')
+        _, parts = projects._get_composite_term_separator_parts(term)
+        for part in parts:
+            resolved_term = projects._resolve_term(part, universe_session, project_session)
+            if resolved_term.kind == TermKind.PATTERN:
+                result += resolved_term.specs[PATTERN_JSON_KEY]
+            else:
+                raise NotImplementedError(f'{term.kind} term is not supported yet')
+    # Patterns terms are meant to be validated individually.
+    # So their regex are defined as a whole (begins by a ^, ends by a $).
+    # As the pattern is a concatenation of plain or regex, multiple ^ and $ can exist.
+    # The later, must be removed.
+    result = result.replace('^', '').replace('$', '')
+    result = f'^{result}$'
     return result
 
 
-def _match_collection(field: str, collections: list[Collection]) -> tuple[str | None, str | list | None]:
+def _match_collection(field: str, collections: list[Collection], universe_session: Session,
+                      project_session: Session) -> tuple[str | None, str | list | None]:
     property_value: str | list | None = None
     property_key: str | None = None
     for collection in collections:
@@ -44,7 +57,9 @@ def _match_collection(field: str, collections: list[Collection]) -> tuple[str | 
                                                     selected_field='drs_name')
                     property_key = 'enum'
                 case TermKind.COMPOSITE:
-                    property_value = _process_composite(collection=collection)
+                    property_value = _process_composite(collection=collection,
+                                                        universe_session=universe_session,
+                                                        project_session=project_session)
                     property_key = 'pattern'
                 case _:
                     msg = f'unsupported term kind {collection.term_kind} ' + \
@@ -54,7 +69,8 @@ def _match_collection(field: str, collections: list[Collection]) -> tuple[str | 
     return property_key, property_value
 
 
-def _generate_property(project_id: str, collections: list[Collection], schema_field: str) -> tuple[str, dict]:
+def _generate_property(project_id: str, collections: list[Collection], schema_field: str,
+                       universe_session: Session, project_session: Session) -> tuple[str, dict]:
     key = f'{project_id}{KEY_SEPARATOR}{schema_field}'
     value: dict[str, Any] = dict()
     value['type'] = 'string'
@@ -63,10 +79,16 @@ def _generate_property(project_id: str, collections: list[Collection], schema_fi
     # 1. Process "long name" fields.
     if LONG_NAME_POSTFIX in schema_field:
         recomputed_schema_field = schema_field.removesuffix(LONG_NAME_POSTFIX)
-        property_key, property_value = _match_collection(recomputed_schema_field, collections)
+        property_key, property_value = _match_collection(recomputed_schema_field,
+                                                         collections,
+                                                         universe_session,
+                                                         project_session)
     else:
         # 2. Process schema_fields that are collection ids. (e.g. of sub_experiment_id).
-        property_key, property_value = _match_collection(schema_field, collections)
+        property_key, property_value = _match_collection(schema_field,
+                                                         collections,
+                                                         universe_session,
+                                                         project_session)
         if property_value is None or property_key is None:
             # 3. Process schema_fields that are part of collection ids.
             for collection in collections:
@@ -108,14 +130,17 @@ def generate_json_schema(project_id: str) -> str:
             file_content = file.read()
         root = json.loads(file_content)
         schema_fields = _get_schema_fields(root)
-        connection = projects._get_project_connection(project_id)
         properties = list()
-        with connection.create_session() as session:
-            collections = projects._get_all_collections_in_project(session)
+        # Connection can't be None here.
+        with search.get_universe_session() as universe_session, \
+             projects._get_project_session_with_exception(project_id) as project_session:
+            collections = projects._get_all_collections_in_project(project_session)
             for schema_field in schema_fields:
                 try:
                     property = _generate_property(project_id=project_id, collections=collections,
-                                                  schema_field=schema_field)
+                                                  schema_field=schema_field,
+                                                  universe_session=universe_session,
+                                                  project_session=project_session)
                     properties.append(property)
                 except Exception as e:  # DEBUG
                     print(e)
