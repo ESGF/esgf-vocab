1449e857cc628fd32a7bcb7ef512610310945a6d
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-06-04 13:49:37 +0200

    fix : rename spec into specs

--- src/esgvoc/api/data_descriptors/data_specs_version.py
+++ src/esgvoc/api/data_descriptors/data_specs_version.py
@@ -1,5 +1,5 @@
 from esgvoc.api.data_descriptors.data_descriptor import PatternTermDataDescriptor
 
 
-class DataSpecVersion(PatternTermDataDescriptor):
+class DataSpecsVersion(PatternTermDataDescriptor):
     pass
