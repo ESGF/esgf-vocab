468008da4e9a12460d032ed88ed49438df864ae9
Sébastien Gardoll <sebastien@gardoll.fr>
2025-09-11 14:01:46 +0200

    refactor: jsg tests

--- tests/test_js_generator.py
+++ tests/test_js_generator.py
@@ -1,95 +1,172 @@
-import logging
-import warnings
-from dataclasses import dataclass
-from typing import Generator
+import json
 
-import pytest
+from jsonschema import validate
 
-from esgvoc.api import projects
 from esgvoc.apps.jsg import json_schema_generator as jsg
-from esgvoc.core.exceptions import EsgvocNotFoundError
 from tests.api_inputs import project_id  # noqa: F401
 
-_LOGGER = logging.getLogger(__name__)
-
-
-
-@dataclass
-class JSGParameter:
-    project_id: str
-    attribute_name: str
-    expected_values: list[str]
-
-
-JSG_PARAMETERS: list[JSGParameter] = [
-    JSGParameter(
-        "cmip6",
-        "variable_id",
-        ["airmass", "albc", "chlcalc"],
-    ),
-    JSGParameter(
-        "cmip6",
-        "grid_label",
-        ["gr6g", "gr9a", "grz"],
-    ),
-    JSGParameter("cmip6", "sub_experiment_id", ["none", "s1976", "s1974"]),
-    JSGParameter(
-        "cmip6",
-        "grid",
+json_example = json.loads(
+"""
+{
+  "type": "Feature",
+  "stac_version": "1.1.0",
+  "stac_extensions": [
+    "https://stac-extensions.github.io/cmip6/v3.0.0/schema.json",
+    "https://stac-extensions.github.io/file/v2.1.0/schema.json"
+  ],
+  "id": "CMIP6.ScenarioMIP.THU.CIESM.ssp585.r1i1p1f1.Amon.rsus.gr.v20200806",
+  "collection": "CMIP6",
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
         [
-            "data reported on a model's native grid",
-            "global mean data",
-            "regridded data reported on the data provider's preferred target grid",
+          -180,
+          -90
         ],
-    ),
-    JSGParameter(
-        "cmip6",
-        "experiment",
         [
-            "Perturbation from 1850 control using 2014 N2O concentrations",
-            "Historical WMGHG concentrations and NTCF emissions, 1950 halocarbon concentrations",
-            "Against a background of the ScenarioMIP high forcing, reduce cirrus cloud optical depth by a constant amount",
+          178.75,
+          -90
         ],
-    ),
-    JSGParameter("cmip6", "data_specs_version", ["^\\d{2}\\.\\d{2}\\.\\d{2}$"]),
-]
+        [
+          178.75,
+          90
+        ],
+        [
+          -180,
+          90
+        ],
+        [
+          -180,
+          -90
+        ]
+      ]
+    ]
+  },
+  "bbox": [
+    -180,
+    -90,
+    178.75,
+    90
+  ],
+  "properties": {
+    "created": "2025-01-24T14:29:23.741213Z",
+    "end_datetime": "4114-12-16T12:00:00Z",
+    "latest": true,
+    "retracted": false,
+    "size": 138734,
+    "start_datetime": "4029-01-16T12:00:00Z",
+    "title": "This is my dataset",
+    "updated": "2025-01-24T14:29:23.741213Z",
+    "cmip6:version": "20250724",
+    "cmip6:activity_id": [
+      "CMIP",
+      "PAMIP"
+    ],
+    "cmip6:citation_url": "http://cera-www.dkrz.de/WDCC/meta/CMIP6/CMIP6.ScenarioMIP.THU.CIESM.ssp585.r1i1p1f1.Amon.rsus.gr.v20200806.json",
+    "cmip6:conventions": [
+      "CF-1.7",
+      "CMIP-6.2"
+    ],
+    "cmip6:data_specs_version": "01.00.29",
+    "cmip6:experiment": "CMIP6 historical (CO2 emission-driven)",
+    "cmip6:experiment_id": "ssp585",
+    "cmip6:forcing_index": 1,
+    "cmip6:frequency": "mon",
+    "cmip6:further_info_url": "https://furtherinfo.es-doc.org/CMIP6.THU.CIESM.ssp585.none.r1i1p1f1",
+    "cmip6:grid": "zonal mean data reported on a model's native latitude grid",
+    "cmip6:grid_label": "gr",
+    "cmip6:initialization_index": 1,
+    "cmip6:institution": "National Taiwan University, Taipei 10650, Taiwan",
+    "cmip6:institution_id": "THU",
+    "cmip6:license": "CC-BY-4.0",
+    "cmip6:member_id": "s1989-r1i1p1f1",
+    "cmip6:nominal_resolution": "100 km",
+    "cmip6:physics_index": 1,
+    "cmip6:pid": "hdl:21.14100/7a8097a5-3ebb-4491-8640-01843dbdecd2",
+    "cmip6:product": "model-output",
+    "cmip6:realization_index": 1,
+    "cmip6:realm": [
+      "atmos"
+    ],
+    "cmip6:source": "NUIST ESM v3",
+    "cmip6:source_id": "CIESM",
+    "cmip6:source_type": [
+      "AOGCM"
+    ],
+    "cmip6:sub_experiment": "initialized near end of year 1950",
+    "cmip6:sub_experiment_id": "s1989",
+    "cmip6:table_id": "Amon",
+    "cmip6:variable_cf_standard_name": "surface_upwelling_shortwave_flux_in_air",
+    "cmip6:variable_id": "rsus",
+    "cmip6:variable_long_name": "Surface Upwelling Shortwave Radiation",
+    "cmip6:variable_units": "W m-2",
+    "cmip6:variant_label": "r1i1p1f1"
+  },
+  "links": [
+    {
+      "rel": "self",
+      "type": "application/geo+json",
+      "href": "https://api.stac.ceda.ac.uk/collections/cmip6/items/CMIP6.ScenarioMIP.THU.CIESM.ssp585.r1i1p1f1.Amon.rsus.gr.v20200806"
+    },
+    {
+      "rel": "parent",
+      "type": "application/json",
+      "href": "https://api.stac.ceda.ac.uk/collections/cmip6"
+    },
+    {
+      "rel": "collection",
+      "type": "application/json",
+      "href": "https://api.stac.ceda.ac.uk/collections/cmip6"
+    },
+    {
+      "rel": "root",
+      "type": "application/json",
+      "href": "https://api.stac.ceda.ac.uk/"
+    }
+  ],
+  "assets": {
+    "reference_file": {
+      "href": "https://dap.ceda.ac.uk/badc/cmip6/metadata/kerchunk/pipeline1/ScenarioMIP/THU/CIESM/kr1.0/CMIP6_ScenarioMIP_THU_CIESM_ssp585_r1i1p1f1_Amon_rsus_gr_v20200806_kr1.0.json",
+      "type": "application/zstd",
+      "created": "2025-01-24T14:29:23.741213Z",
+      "file:checksum": "90e402107a7f2588a85362b9beea2a12d4514d45",
+      "file:size": 12345,
+      "protocol": "https",
+      "updated": "2025-01-24T14:29:23.741213Z",
+      "cmip6:tracking_id": "hdl:21.14100/7a8097a5-3ebb-4491-8640-01843dbdecd2",
+      "roles": [
+        "replica",
+        "data"
+      ],
+      "open_zarr_kwargs": {
+        "decode_times": true
+      }
+    },
+    "data0001": {
+      "href": "https://dap.ceda.ac.uk/badc/cmip6/data/CMIP6/ScenarioMIP/THU/CIESM/ssp585/r1i1p1f1/Amon/rsus/gr/v20200806/rsus_Amon_CIESM_ssp585_r1i1p1f1_gr_402901-411412.nc",
+      "type": "application/netcdf",
+      "created": "2025-01-24T14:29:23.741213Z",
+      "file:checksum": "90e402107a7f2588a85362b9beea2a12d4514d45",
+      "file:size": 23553,
+      "protocol": "s3",
+      "updated": "2025-01-24T14:29:23.741213Z",
+      "cmip6:tracking_id": "hdl:21.14100/7a8097a5-3ebb-4491-8640-01843dbdecd2",
+      "roles": [
+        "data"
+      ]
+    }
+  }
+}
+""")
 
 
-def _provide_get_jsg_parameters() -> Generator:
-    for param in JSG_PARAMETERS:
-        yield param
+def test_compliance() -> None:
+    json_schema = jsg.generate_json_schema('cmip6')
+    validate(instance=json_example, schema=json_schema)
 
-
-@pytest.fixture(params=_provide_get_jsg_parameters())
-def jsg_parameter(request) -> JSGParameter:
-    return request.param
-
-
-def test_generate_property(jsg_parameter) -> None:
-    project_specs = projects.get_project(jsg_parameter.project_id)
-    assert project_specs
-    assert project_specs.global_attributes_specs
-    with jsg.JsonPropertiesVisitor(jsg_parameter.project_id) as visitor:
-        assert jsg_parameter.attribute_name in project_specs.global_attributes_specs
-        attribute = project_specs.global_attributes_specs[jsg_parameter.attribute_name]
-        attribute_key, attribute_properties = attribute.accept(jsg_parameter.attribute_name, visitor)
-        for expected_value in jsg_parameter.expected_values:
-            if "enum" in attribute_properties:
-                assert "enum" in attribute_properties
-                assert expected_value in attribute_properties["enum"]
-            else:
-                assert "pattern" in attribute_properties
-                assert expected_value in attribute_properties["pattern"]
 
 
 def test_js_generation(project_id) -> None:
-    try:
-        js = jsg.generate_json_schema(project_id)
-        assert js
-    except EsgvocNotFoundError as e:
-        if "template" in str(e):
-            _LOGGER.warning(f"json schema for project {project_id} not found. Escape")
-            warnings.warn(f"json schema for project {project_id} not found. Escape",
-                          stacklevel=1)
-        else:
-            raise e
+    js = jsg.generate_json_schema(project_id)
+    assert js
