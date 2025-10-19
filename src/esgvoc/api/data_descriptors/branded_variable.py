788cbc52a4d703513466def18aadb17c80fc9c97
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-19 07:02:16 +0200

    Clean up branded variable defn

--- src/esgvoc/api/data_descriptors/branded_variable.py
+++ src/esgvoc/api/data_descriptors/branded_variable.py
@@ -1,3 +1,7 @@
+"""
+Model (i.e. schema/definition) of the branded variale data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import CompositeTermDataDescriptor
 
 
@@ -10,8 +14,6 @@ class BrandedVariable(CompositeTermDataDescriptor):
     The first part is the root variable (see :py:class:`Variable`).
     The second is the suffix (see :py:class:`BrandedSuffix`).
 
-    For further details on the development of branded variables,
-    see [this paper draft](https://docs.google.com/document/d/19jzecgymgiiEsTDzaaqeLP6pTvLT-NzCMaq-wu-QoOc/edit?pli=1&tab=t.0).
-    """
-
-    description: str
+    For underlying details and logic, please see
+    [Taylor et al., 2025](https://docs.google.com/document/d/19jzecgymgiiEsTDzaaqeLP6pTvLT-NzCMaq-wu-QoOc/edit?pli=1&tab=t.0).
+    """  # noqa: E501
