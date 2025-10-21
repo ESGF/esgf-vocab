4c3992b8ed3e539f8f628dd5c8dedc8a9d3f97b5
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-21 12:25:27 +0200

    Update conventions

--- src/esgvoc/api/data_descriptors/conventions.py
+++ src/esgvoc/api/data_descriptors/conventions.py
@@ -1,6 +1,22 @@
+"""
+Model (i.e. schema/definition) of the conventions data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
 class Convention(PlainTermDataDescriptor):
-    description: str
+    """
+    CF version governing the data
+
+    Examples: "CF-1.10", "CF-1.12"
+
+    Climate and forecast metadata conventions (https://cfconventions.org/)
+    that the data follows.
+    This data descriptor is actually defined by the CF-conventions.
+    However, it is often used in a more specific and restrictive form
+    within WCRP activities, which is why we have to define it within esgvoc.
+    """
+
+    # TODO: delete given we have description and drs_name
     label: str
