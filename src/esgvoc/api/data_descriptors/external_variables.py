7c611e11db586fd1b6c15d3061b7532bf90f6fd1
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-23 15:01:52 +0200

    Add model

---
+++ src/esgvoc/api/data_descriptors/external_variables.py
@@ -0,0 +1,17 @@
+"""
+Model (i.e. schema/definition) of the external variables data descriptor
+"""
+
+from esgvoc.api.data_descriptors.data_descriptor import PatternTermDataDescriptor
+
+
+class ExternalVariables(PatternTermDataDescriptor):
+    """
+    Variables relevant to a dataset that are stored external to the dataset
+
+    Examples: "areacella", "areacella sftlf", "areacello sftof", "volcello"
+
+    Governed by CF-conventions
+    (https://cfconventions.org/cf-conventions/cf-conventions.html#external-variables).
+    This is only required if `cell_measures` are specified.
+    """
