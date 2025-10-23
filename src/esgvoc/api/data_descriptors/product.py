8636a578fcbbc208a4f29c5df4a6970da5bc0954
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-23 05:40:15 +0200

    Update models

--- src/esgvoc/api/data_descriptors/product.py
+++ src/esgvoc/api/data_descriptors/product.py
@@ -1,5 +1,16 @@
+"""
+Model (i.e. schema/definition) of the product data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
 class Product(PlainTermDataDescriptor):
-    kind: str
+    """
+    Identifier of the data category
+
+    Examples: "model-output", "observations", derived"
+
+    This is not a precisely defined data descriptor,
+    rather an approximate labelling.
+    """
