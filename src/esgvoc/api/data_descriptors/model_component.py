ac3aca9ecbcfd8f37e9289653bb8f8981f5e2f4a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-24 16:12:24 +0200

    Update model component

--- src/esgvoc/api/data_descriptors/model_component.py
+++ src/esgvoc/api/data_descriptors/model_component.py
@@ -1,8 +1,19 @@
+"""
+Model (i.e. schema/definition) of the model component data descriptor
+"""
+
 from esgvoc.api.data_descriptors.data_descriptor import PlainTermDataDescriptor
 
 
 class ModelComponent(PlainTermDataDescriptor):
-    name: str
-    realm: dict
-    nominal_resolution: dict
-    version: int
+    """
+    Model component
+
+    Examples: "AOGCM", "AER", "BGC"
+
+    These terms are intended to help with identifying required components for experiments
+    or filtering models based on having common components.
+    For example, an aerosol scheme or a circulation model or a biogeochemistry component.
+    However, model component is only an approximate term, there is no precise definition
+    of whether any given model has or does not have a given component.
+    """
