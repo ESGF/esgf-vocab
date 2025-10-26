159d2477a212021914ed9660e9d494e35a494f41
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-26 10:31:47 +0100

    Update data specs version docstring

--- src/esgvoc/api/data_descriptors/data_specs_version.py
+++ src/esgvoc/api/data_descriptors/data_specs_version.py
@@ -15,6 +15,11 @@ class DataSpecsVersion(PlainTermDataDescriptor):
     used when writing the dataset.
     This version number captures exactly which set of data specifications
     are consistent (or intended to be consistent) with this dataset.
+    The DRS values can't contain '.' so we use 'p' instead.
+    To go from a DRS value back to a standard version,
+    get everything after the hyphen (everything before the hyphen is a prefix)
+    then replace "p" with ".".
+    Something like, `drs_name.split('-')[-1].replace('p', '.')`.
     (At the moment, exactly what this means is still vague, particularly for CMIP7.
     When it solidifies, more details and examples will be added here.)
     """
