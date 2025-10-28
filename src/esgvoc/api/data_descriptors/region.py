2008d10cd0e0f4437ac71701efb9b3c4e2c2a9f5
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-10-28 08:12:57 +0100

    Add cf standard region and iso attributes

--- src/esgvoc/api/data_descriptors/region.py
+++ src/esgvoc/api/data_descriptors/region.py
@@ -9,9 +9,27 @@ class Region(PlainTermDataDescriptor):
     """
     Region associated with the dataset
 
-    Examples: "global", "great_lakes", "indian_ocean"
+    Examples: "glb", "30s-90s", "grl"
 
     In other words, the domain over which the dataset is provided.
     This is intended as a rough categorisation only
     and is not precisely defined.
+    """
+
+    cf_standard_region: str | None
+    """
+    CF standard region
+
+    See https://cfconventions.org/Data/standardized-region-list/standardized-region-list.current.html
+
+    If `None`, there is no CF standard region for this region
+    """
+
+    iso_region: str | None
+    """
+    ISO 3166-1 alpha-3 region code
+
+    See https://www.iso.org/iso-3166-country-codes.html
+
+    If `None`, there is no ISO region code for this region
     """
