f805e357a52343fa84eb7026da6bfbffbe5fc3e5
Sébastien Gardoll <sebastien@gardoll.fr>
2025-11-05 18:15:49 +0100

    fix: support hyphen in extension names

--- src/esgvoc/apps/jsg/json_schema_generator.py
+++ src/esgvoc/apps/jsg/json_schema_generator.py
@@ -296,7 +296,8 @@ def generate_json_schema(project_id: str) -> dict:
             template = env.get_template(TEMPLATE_FILE_NAME)
             extension_specs = dict()
             for catalog_extension in catalog_specs.catalog_properties.extensions:
-                extension_specs[f'{catalog_extension.name}_extension_version'] = catalog_extension.version
+                catalog_extension_name = catalog_extension.name.replace('-', '_')
+                extension_specs[f'{catalog_extension_name}_extension_version'] = catalog_extension.version
             drs_dataset_id_regex = project_specs.drs_specs[DrsType.DATASET_ID].regex
             property_translator = CatalogPropertiesJsonTranslator(project_id)
             catalog_dataset_properties = \
