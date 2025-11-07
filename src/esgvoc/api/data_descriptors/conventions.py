070d93240ac1ea1218ff6389d73c933a9236a80b
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-11-07 23:01:28 +0800

    Update conventions with note

--- src/esgvoc/api/data_descriptors/conventions.py
+++ src/esgvoc/api/data_descriptors/conventions.py
@@ -5,6 +5,8 @@ Model (i.e. schema/definition) of the conventions data descriptor
 from esgvoc.api.data_descriptors.data_descriptor import PatternTermDataDescriptor
 
 
+# TODO: revert to PlainTermDataDescriptor.
+# See https://github.com/ESGF/esgf-vocab/pull/168#discussion_r2499193117
 class Convention(PatternTermDataDescriptor):
     """
     CF version governing the data
