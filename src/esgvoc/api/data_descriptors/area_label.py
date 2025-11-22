63ed45128adeb2b9332ddf20635a6685144cc57a
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-11-22 17:16:38 +1100

    Add CF convention attribute to area label

--- src/esgvoc/api/data_descriptors/area_label.py
+++ src/esgvoc/api/data_descriptors/area_label.py
@@ -11,13 +11,20 @@ class AreaLabel(PlainTermDataDescriptor):
 
     Examples: "lnd", "air", "sea", "u"
 
-    The values are defined by the CF-conventions area types
+    This label is used as the area component of a branded variable's suffix
+    (see :py:class:`BrandedSuffix`).
+    """
+
+    cf_area_type: str | None
+    """
+    CF-conventions area type
     (https://cfconventions.org/Data/area-type-table/current/build/area-type-table.html).
+
     This is set to "u" ("unmasked") when all areas are sampled
     i.e. no mask is applied to the data.
     For underlying details and logic, please see
     [Taylor et al., 2025](https://docs.google.com/document/d/19jzecgymgiiEsTDzaaqeLP6pTvLT-NzCMaq-wu-QoOc/edit?pli=1&tab=t.0).
 
-    This label is used as the area component of a branded variable's suffix
-    (see :py:class:`BrandedSuffix`).
+    If `None`, there is no CF-conventions area type
+    associated with this area label.
     """  # noqa: E501
